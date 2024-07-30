fec8d0621019e7d1d197636139643bf134c0aebf
Stoyan Radichev <stoyan.radichev.a62@learn.telerikacademy.com>
2024-07-30 19:17:33 +0100

    Add files via upload

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+  // const ButtonContainer = buttonsContainer;
+  return (
+    <>
+      <ButtonsContainer>
+        {buttons}
+      </ButtonsContainer>
+      {children}
+    </>
+  )
+}
